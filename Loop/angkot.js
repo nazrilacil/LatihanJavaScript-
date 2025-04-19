9d2da86a44eb64627c396e27ad9f60a733792808
Nazril Acil <nazrilacil04@gmail.com>
2025-04-19 08:40:30 +0700

    While & For

---
+++ Loop/angkot.js
@@ -0,0 +1,11 @@
+let noAngkot = 1;
+let angkotBeroprasi = 6;
+let jumlahAngkot= 10;
+
+while(noAngkot <= angkotBeroprasi) {
+     console.log(`Angkot No. ${noAngkot} Beroperasi dengan baik.`);
+     noAngkot++
+}
+for(noAngkot = angkotBeroperasi + 1;noAngkot <= jumlahAngkot; noAngkot++) {
+    console.log(`Angkot No. ${noAngkot} Tidak Beroperasi`);
+}
